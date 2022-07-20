198865b216b76cec13c321a8f20f7778324f70f8
seaprison51 <huutai.2021tn@gmail.com>
2022-07-20 22:49:31 +0700

    add file again

--- duan1.py
+++ duan1.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 
 root = tk.Tk()
-
-label = tk.Label(root, text="Hello World", padx=10, pady=10)
+root.title("BMI")
+root.geometry("800x600")
+label = tk.Label(root, text="xin ckào", fg="red", font=("Arial", 60))
 label.pack()
 
 root.mainloop()
