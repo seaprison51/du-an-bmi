d3baa4c03c20e6b3ddfad95b866316fe857c39b0
seaprison51 <huutai.2021tn@gmail.com>
2022-07-21 21:30:23 +0700

    code nham

--- nhap01.py
+++ nhap01.py
@@ -1,4 +1,9 @@
 print("hello world")
 for i in range(0, 10):
-  print("good night")
+    print("good night")
 print("wtf gi zay")
+a = "aaaahhhhhh"
+b = "dau daau qua"
+ab = (a, b)
+for i in ab:
+    print(i, "!!!!")
