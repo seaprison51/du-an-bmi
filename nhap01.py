6405c7bb84ac11eb542dbfff4742e746d9cbb5e7
seaprison51 <107759337+seaprison51@users.noreply.github.com>
2022-07-21 21:26:25 +0700

    Update nhap01.py

--- nhap01.py
+++ nhap01.py
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("thuwr file")
-print("ok oonr aps")
-print("nhma van hoi la")
-print("kkkk")
-for x in range(0, 10):
-    print("mng ngur ngon")
-print("oke good job")
-=======
-for i in range(0, 10):
-  print("camonvitatca")
-print("end")
->>>>>>> b880acfb711d4d33ead0be5923b04355f3737a44
-=======
 print("hello world")
->>>>>>> 1c1aeacd6903e96864f9d4dc053b8d014b35fde1
+for i in range(0, 10):
+  print("good night")
