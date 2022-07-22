1a29339a90a58b87b93add90c56aefae332e62a6
seaprison51 <huutai.2021tn@gmail.com>
2022-07-22 22:51:39 +0700

    xoa het

--- duan1.py
+++ duan1.py
@@ -1,9 +1,1 @@
-import tkinter as tk
-
-root = tk.Tk()
-root.title("BMI")
-root.geometry("800x600")
-label = tk.Label(root, text="xin ckào", fg="red", font=("Arial", 60))
-label.pack()
-
-root.mainloop()
+print("abc123")
